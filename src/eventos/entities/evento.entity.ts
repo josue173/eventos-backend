0de79360c333e34fd79e9154a2b8d27bb5e84f36
josue173 <josue.perez04@outlook.com>
2025-10-13 16:41:12 -0600

    feat: inicio

---
+++ src/eventos/entities/evento.entity.ts
@@ -0,0 +1,21 @@
+export class Evento {
+  ev_id: string;
+
+  ev_nombre: string;
+
+  ev_descripcion: string;
+
+  ev_fecha_creacion: Date;
+
+  ev_fecha_evento: Date;
+
+  ev_fecha_modificacion: Date;
+
+  ev_lugar: string;
+
+  ev_hora_inicio: string;
+
+  ev_hora_final: string;
+
+  ev_
+}
